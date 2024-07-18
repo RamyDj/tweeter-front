153664c863c23fa91120172c09e84cae9dc10e88
Jul1enF <j.furic@gmail.com>
2024-07-18 16:09:38 +0200

    ajout de l'import de lasttweets dans home

--- components/Tweet.js
+++ components/Tweet.js
@@ -8,7 +8,7 @@ function Tweet () {
 
     return (
         <div className={styles.body}>
-            <input type='texte' placeholder="What's up?" className={styles.tweetInput} onChange={e=>setTweetTyped(e.target.value)} value={tweetTypped}></input>
+            <input type='texte' placeholder="What's up?" className={styles.tweetInput} onChange={e=>{if (e.target.value.length<281){setTweetTyped(e.target.value)}}} value={tweetTypped}></input>
             <div className={styles.postInfosContainer}>
                 <span className={styles.characterCount}>{tweetTypped.length}/280</span>
                 <button className={styles.tweetButton}>Tweet</button>
