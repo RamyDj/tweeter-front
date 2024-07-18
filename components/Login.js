7353fca0deaec899f344c88b84b80db7294f3bfc
Ramy <ramyto.rd@gmail.com>
2024-07-18 12:51:21 +0200

    changement nom css login

--- components/Login.js
+++ components/Login.js
@@ -1,6 +1,30 @@
-import styles from '../styles/Home.module.css';
+import styles from '../styles/Login.module.css';
+import { useState, useEffect } from "react";
+import { useRouter } from 'next/router';
 
 function Login() {
+  const [SignInModal, setSignInModal] = useState(false);
+  const [SignUpModal, setSignUpModal] = useState(false);
+
+  const router = useRouter();
+
+  const opensignUpModal = () => {
+    setSignUpModal(true);
+    setSignInModal(false)
+  };
+
+  const closesignUpModal = () => {
+    setSignUpModal(false);
+  };
+
+  const opensignInModal = () => {
+    setSignUpModal(false);
+    setSignInModal(true)
+  };
+
+  const closesignInModal = () => {
+    setSignInModal(false)
+  };
   return (
     <div>
       <main className={styles.main}>
