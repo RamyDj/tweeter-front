d06fdd45c73a02cad556a06a924ed206e208068e
Irene <gallibour.irene@gmail.com>
2024-07-19 12:13:18 +0200

    trends.js manque nb tweet/redirection vers hashtag fait et page hashtag fait

---
+++ components/Hashtag.js
@@ -0,0 +1,62 @@
+import styles from '../styles/Home.module.css'
+import Image from 'next/image'
+import Tweet from './Tweet'
+import LastTweets from './LastTweets'
+import { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import Trends from './Trends';
+import { useRouter } from 'next/router';
+
+
+function Hashtag (){
+    const [tweets, setTweets] = useState([]);
+    
+    useEffect(() => {
+        fetch('http://localhost:3000/tweet') //a modifier avec route hashtag
+            .then(response => response.json())
+            .then(data => {console.log(data);
+                setTweets(data.allTweet); 
+                
+            })
+    }, []);
+
+return(    
+<div className={styles.body}>
+    <div className={styles.leftContainer}>
+        <div className={styles.tweeterLogoContainer}>
+            <Image src='/logo.png' alt='Logo Twitter' layout='fill'/>
+        </div>
+        <div className={styles.userInfosContainer}>
+            <div className={styles.userLogoContainer}>
+                <Image src='/egg.jpg' alt='User logo' layout='fill' className={styles.userLogo}/>
+            </div>
+        </div>
+        
+    </div>
+    <div className={styles.centralContainer}>
+        <div className={styles.tweetContainer}>
+            <h2 className={styles.title}>#hashtag</h2>
+            <Tweet />
+        </div>
+        <div className={styles.lastTweetsContainer}>
+        <ul>
+                {tweets.map(tweet => (
+                    <li key={tweet._id}>
+                        <strong>{tweet.username}</strong>: {tweet.message}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    </div>
+    <div className={styles.rightContainer}>
+        <h2 className={styles.title}>Trends</h2>
+        <div className={styles.TrendsContainer}>
+            <Trends tweets={tweets} />
+        </div>
+    </div>
+
+</div>
+)
+}
+
+export default Hashtag
--- components/Trends.js
+++ components/Trends.js
@@ -1,16 +1,46 @@
+import { useState, useEffect } from "react";
+import { useRouter } from "next/router";
+
 function Trends(props) {
+    const [hashtags, setHashtags] = useState([]);
+    const router = useRouter()
 
-    const handleTweet = () =>{
+    useEffect(() => {
+        if (props.tweets.length > 0) {
+            const allHashtags = props.tweets.map(tweet => tweet.hashtag);
 
+            let flatHashtags = [];
+            for (const hashtagsArray of allHashtags) {
+                flatHashtags = flatHashtags.concat(hashtagsArray);
+            }
+            const uniqueHashtags = [...new Set(flatHashtags)];
+            const filteredHashtags = uniqueHashtags.filter(hashtag => hashtag !== null);
+
+            setHashtags(filteredHashtags);
+        }
+    }, [props.tweets]);
+
+
+    console.log(props.tweets)
+
+    const hashtagView = hashtags.map((hashtag, i) => (
+        <li key={i}>{hashtag}</li>
+    ));
+
+
+    const handleClickToHashtag = () =>{
+        router.push("/hashtag")
     }
+    
+//A faire => nombre de tweet par hashtag
 
-
-    return(
-         <div onClick={() => handleTweet()}> 
-            <h3>Hashtag</h3> 
-            <p>Nb Tweets</p>
+    return (
+        <div onClick={() => handleClickToHashtag()}>
+            <div>{hashtagView}
+                <span>Nb Tweets</span> 
+            </div>
         </div>
-    )
+    );
 }
 
-export default Trends
+export default Trends;
